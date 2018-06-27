5dbb1080a9d503a6eb8b699d649f1c3665d9d13b
Dan <dantu22@live.com>
2018-06-27 10:38:31 -0700

    Added Firebase access and document ready function

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-"use strict";
-
-var database, storage, auth, messagesRef;
-const textInput = document.getElementById("message-input");
-const messages = document.getElementById("messages");
-
 // Init Firebase
 var config = {
     apiKey: "AIzaSyDrwH-mQ3ketttl4EL6gt2G7hhG_3UhbK4",
@@ -12,54 +6,8 @@ var config = {
     projectId: "chatapp-a0804",
     storageBucket: "chatapp-a0804.appspot.com",
 };
+firebase.initializeApp(config);
 
-// Sets Firebase shortcuts for ease of use
-var initFirebase = function() {
-    database = firebase.database();
-    storage = firebase.storage();
-    auth = firebase.auth();
-}
-
-// Takes a text and adds it to the content-container
-var addMessage = function(text) {
-    var messageContainer = document.createElement('div');
-    messageContainer.innerHTML = text;
-    messages.appendChild(messageContainer);
-}
-
-// Loads all the messages currently stored in Firebase DB
-var loadMessages = function() {
-    messagesRef = database.ref("Messages");
-    messagesRef.off();
-    
-    var setMessage = function(data) {
-        var val = data.val();
-        addMessage(val.text);
-    };
-
-    messagesRef.on('child_added', setMessage);
-}
-
-// Pushes message to firebase then clears text field
-var submitMessage = function(e) {
-    e.preventDefault();
-    if (textInput.value) {
-        messagesRef.push({
-            text: textInput.value
-        }).then(() => {
-            $("#message-input").val('');
-        });
-    }
-}
-
-function readyPage() {
-    firebase.initializeApp(config);
-    initFirebase();
-    loadMessages();
-}
-
-$("document").ready(readyPage())
-
-$("#input-form").submit(submitMessage);
-
-
+$("document").ready(() => {
+    console.log(firebase.database());
+});
