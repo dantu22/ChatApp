b3167affadac502f69619b25fd89bcbe7d92a45c
Dan <dantu22@live.com>
2018-07-11 10:08:21 -0700

    Moving files around

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
 
-app.get('/', (req, res) => res.send("Hello world!"));
